e2c02efade424952ad4c167185563b51ca318145
skurtbas <sedat.kurtbas@certusystems.com>
2025-11-13 16:12:22 -0500

    sep practice

--- tests/day05/sep-practice.spec.js
+++ tests/day05/sep-practice.spec.js
@@ -14,4 +14,27 @@ test("SEP Practice @sep", async ({ page }) => {
   // await page.goto("https://qa.sep.tdtm.cydeo.com/taws");
   await page.goto(`${process.env.SEP_QA_URL}`);
   await page.waitForTimeout(1000);
+
+  // Start Application Step:
+  let firstNameInput = page.locator("#mat-input-0");
+  await firstNameInput.fill("Haci");
+
+  let lastNameInput = page.locator("#mat-input-1");
+  await lastNameInput.fill("Haciogullarindan");
+
+  let emailAddressInput = page.locator("#mat-input-2");
+  await emailAddressInput.fill("hacihaciogullarindan@haci.com");
+
+  let phoneNumberInput = page.locator("#mat-input-3");
+  await phoneNumberInput.fill("5555555");
+
+  let howDidYouDropdown = page.getByText("How did you hear");
+  await howDidYouDropdown.click();
+
+  await page.click("//span[text()='Email']");
+
+  let nextButton = page.locator("//button[@type='submit']");
+  await nextButton.click();
+
+  await page.waitForTimeout(2000);
 });
