9d4c4d72ed5a11b80af6360678112c02ce9f8d80
skurtbas <sedat.kurtbas@certusystems.com>
2025-11-14 21:40:03 -0500

    iframe updated

--- tests/day04/iframes.spec.js
+++ tests/day04/iframes.spec.js
@@ -2,17 +2,17 @@ import { test, expect } from "@playwright/test";
 
 test("iFrame test", async ({ page }) => {
   await page.goto("https://practice.cydeo.com/iframe");
-  let myFrame = page.frameLocator("#mce_0_ifr");
-  let elementInsideTheFrame = myFrame.locator("#tinymce");
+  // let myFrame = page.frameLocator("#mce_0_ifr");
+  // let elementInsideTheFrame = myFrame.locator("#tinymce");
 
   // await page.waitForTimeout(2000);
 
-  await elementInsideTheFrame.clear();
-  // await elementInsideTheFrame.press("Control+A", "delete");
+  const frame = page.frameLocator("#mce_0_ifr");
+  const editor = frame.locator("#tinymce");
 
-  await elementInsideTheFrame.fill("Hello World!");
+  // Make the editor editable if needed
+  await editor.evaluate((el) => el.setAttribute("contenteditable", "true"));
 
-  // await page.waitForTimeout(2000);
-
-  await expect(elementInsideTheFrame).toHaveText("Hello World!");
+  await editor.fill("Hello World!");
+  await expect(editor).toHaveText("Hello World!");
 });
