44aefedc8ef5415bd81315d45f063c11a60811ea
skurtbas <sedat.kurtbas@certusystems.com>
2025-11-14 21:21:54 -0500

    environment variables updated

--- tests/day03/environment-variables.spec.js
+++ tests/day03/environment-variables.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test("@env-test environment variables", async ({ page }) => {
   console.log(`username is ${process.env.PRACTICE_USERNAME}`);
@@ -8,11 +8,12 @@ test("@env-test environment variables", async ({ page }) => {
 test("Bypass authentication by encoding the credentials base64 format", async ({
   page,
 }) => {
-  let username = process.env.PRACTICE_USERNAME;
-  let password = process.env.PRACTICE_PASSWORD;  
-  let encodedCredential = Buffer.from("username:password").toString("base64");
-  await page.setExtraHTTPHeaders({ Authorization: `Basic ${encodedCredential}` });
-  await page.goto("https://practice.cydeo.com/basic_auth");
+  let encodedCredential = Buffer.from(
+    `${process.env.PRACTICE_USERNAME}:${process.env.PRACTICE_PASSWORD}`
+  ).toString("base64");
+  await page.setExtraHTTPHeaders({
+    Authorization: `Basic ${encodedCredential}`,
+  });
+  page.goto("https://practice.cydeo.com/basic_auth");
   await page.waitForTimeout(3000);
-
 });
